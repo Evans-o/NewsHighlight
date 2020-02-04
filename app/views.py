109324008d79f35f5b251de5b2bd734d57faa127
Evans-o <evansopindi@yahoo.com>
2020-02-04 15:59:24 +0300

    added initial structures

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from app import app
+from .requests import get_news,get_news
 
 @app.route('/')
 def index():
@@ -7,9 +8,21 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    # Getting popular movie
+    # Getting popular news
     popular_news = get_news('popular')
     upcoming_news = get_news('upcoming')
     now_showing_news = get_news('now_showing')
     title = 'Home - Welcome to The best News Review Website Online'
     return render_template('index.html', title = title, popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news )
+
+    @app.route('/news/<int:id>')
+def news(id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    news = get_news(id)
+    title = f'{news.title}'
+
+    return render_template('news.html',title = title,news = news)
+    
